f95274a0425ba06168f357096ff2e172e2367247
Jimmy Alejandro Tzuc Martin <stjimmytzuc@gmail.com>
2022-10-03 17:00:14 -0500

    Created model book

--- core/plataforma/models.py
+++ core/plataforma/models.py
@@ -1,3 +1,9 @@
+from enum import auto
 from django.db import models
 
 # Create your models here.
+class RoomModel(models.Model):
+    capacity = models.IntegerField()
+    date = models.DateTimeField( auto_now=False, auto_now_add=False)
+    created = models.DateTimeField(auto_now_add = True)
+    updated = models.DateTimeField(auto_now = True)
